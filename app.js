7996455b744b4bfb19da6df293efc880beedbb09
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2021-09-07 12:41:07 -0400

    setup boilerplated layout

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const path = require('path');
-
+const ejsMate = require('ejs-mate');
 const app = express();
-
+app.engine('ejs', ejsMate)
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
@@ -25,6 +25,8 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
+
+
 
 app.get('/', (req, res) => {
     res.render('home')
