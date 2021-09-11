815049cca51dfe1c4d1dc9b2b991e439fa9fe9c3
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2021-09-11 19:33:35 -0400

    group similar routes

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -16,4 +17,13 @@ const CampgroundSchema = new Schema({
     ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 module.exports = mongoose.model('Campground', CampgroundSchema)
