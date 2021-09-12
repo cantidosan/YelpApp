2f5a7412530caaa1c91c94d65ce63c16e634406f
Cantidosan <82469261+cantidosan@users.noreply.github.com>
2021-09-11 20:14:31 -0400

    add comments and documentation

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 
+//launches the webserver
 const express = require('express');
+const app = express();
+
+//creates the file structure for the website
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const app = express();
+
+//handles parsing the ejs,.js file extensions
 app.engine('ejs', ejsMate)
+/// used for server side validation
 const Joi = require('joi');
 
-const session = require('express-session');
-const ExpressError = require('./utils/ExpressError');
+
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+//CREATES COOKIEs to save states
+const session = require('express-session');
+
+//custom errorclass for error handling
+const ExpressError = require('./utils/ExpressError');
+
+//database instances
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
+//decoupled Rest routes?
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews')
 
@@ -23,19 +36,23 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useUnifiedTopology: true */
 });
 
+//basic db connection setup
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
+///cookie session details
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false,
@@ -49,6 +66,7 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 
 
+
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 
@@ -56,7 +74,6 @@ app.use('/campgrounds/:id/reviews', reviews)
 app.get('/', (req, res) => {
     res.render('home')
 });
-
 
 
 app.all('*', (req, res, next) => {
